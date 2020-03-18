3adb86e7503e96417b4590f01d670e06467214fa
budyhartanto <you@example.com>
2020-03-18 19:24:41 +0700

    Budy

---
+++ Object.java
@@ -0,0 +1,13 @@
+public class Object{
+
+
+    public static void main(String[] args) {
+        Mahasiswa budy = new Mahasiswa();
+        budy.nim = "18090050";
+        budy.nama = "Muhammad Budy Hartanto";
+        budy.kelas = "4B";
+        System.out.println("Nim : " + budy.nim);
+        System.out.println("Nama : " + budy.nama);
+        System.out.println("Kelas : " + budy.kelas);
+    }
+}
